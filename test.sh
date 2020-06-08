1eccd4005692f34a6d41e6b97469939fbc3c8bb6
AnCh <anton_chug@yahoo.com>
2020-06-08 10:17:09 -0500

    Added test script and more logs

---
+++ test.sh
@@ -0,0 +1,15 @@
+curl --verbose --header 'Host: jsonplaceholder.typicode.com' 'http://localhost:9000/todos/1'
+sleep 1
+curl --verbose --header 'Host: api.coinranking.com' 'http://localhost:9000/v1/public/coin/1'
+sleep 1
+curl --verbose --header 'Host: api.coinranking.com' 'http://localhost:9000/v1/public/coin/1'
+sleep 1
+curl --verbose --header 'Host: api.coinranking.com' 'http://localhost:9000/v1/public/coin/1'
+sleep 1
+curl --verbose --header 'Host: jsonplaceholder.typicode.com' 'http://localhost:9000/todos/1'
+sleep 1
+curl --verbose --header 'Host: jsonplaceholder.typicode.com' 'http://localhost:9000/todos/1'
+sleep 1
+curl --verbose --header 'Host: jsonplaceholder.typicode.com' 'http://localhost:9000/todos/1'
+sleep 1
+curl --verbose --header 'Host: api.coinranking.com' 'http://localhost:9000/v1/public/coin/1'
